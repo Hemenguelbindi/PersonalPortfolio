87e115e6ecc700c5b19dcbba48b40bd1ef5b01be
VictorHemengul <86350703+VictorHemengul@users.noreply.github.com>
2023-01-06 04:03:23 +0500

    Create data base in project

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -10,19 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from environs import Env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = Env()
-env.read()
+env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = evn.str("KEY")
+SECRET_KEY = env("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "blog"
-    "portfolio"
+    "blog",
+    "portfolio",
 ]
 
 MIDDLEWARE = [
@@ -118,9 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+MEDIA_URL = '/media/'
 STATIC_URL = 'static/'
-
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class PorjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image', 'url')
+
+
