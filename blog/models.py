f5218e9acf619283c3b638ac5205ce958ddd0b5f
VictorHemengul <86350703+VictorHemengul@users.noreply.github.com>
2023-01-06 15:41:48 +0500

    Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    date = models.DateTimeField()
+    
+    
